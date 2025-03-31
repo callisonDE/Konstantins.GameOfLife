c3c01878c8b93b1349c1fa2e7043b42ed654a49b
Your Name <you@example.com>
2025-03-31 15:36:32 +0200

    add cells

--- Source/life.js
+++ Source/life.js
@@ -12,14 +12,11 @@ let grid = new Grid();
 
 for(let i = 0; i < 10; i++)
 {
-    let cell = new Cell(true);
-    console.log(cell.isAlive);
-    
-    // grid.setAlive(0, i);
+    let isAlive = (i != 1);
+    let cell = new Cell(isAlive);
+    console.log(cell.isAlive); 
 }
 
 // erste Zellen lebendig machen
 //grid.setAlive(0, 0);
-
-
 // grid.clear();
