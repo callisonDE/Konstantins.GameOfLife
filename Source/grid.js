1e1469b3003d7cca9413a1174636ce36e5fc4c55
Sebastian Felling <sebastian@felling.de>
2025-03-31 12:57:37 +0200

    fix grid clearing

--- Source/grid.js
+++ Source/grid.js
@@ -11,8 +11,9 @@ class Grid
     
     clear()
     {
-        this.hostElement.clear();
+        this.context.clearRect(0, 0, this.hostElement.width, this.hostElement.height);
     }
+    
     setAlive(x, y)
     {
         this.context.fillStyle = "black";
